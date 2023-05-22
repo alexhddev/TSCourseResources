59fac1d783a91badd26da966bc8638bae13bd1a8
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-05-22 12:06:30 +0300

    Primitive types

---
+++ section2/src/2PrimitiveTypes.ts
@@ -0,0 +1,32 @@
+// JS types:
+let fistName = 'John';
+let isAdmin = false;
+let age = 30;
+
+let duties = ['write code', 'fix bugs']
+
+let car = null;
+let bicycle = undefined;
+
+let work = () => {
+    console.log('working...')
+}
+
+let salary = 50n;
+let logo = Symbol('emerald')
+
+let all = [fistName, isAdmin, age, car, bicycle, duties, work, salary, logo];
+
+for (let item of all) {
+    console.log(String(item) + ' is ' + typeof item);
+}
+
+/**
+ * TS types:
+ * any - 
+ * unknown
+ * never
+ * arrays
+ * tuples
+ * Enums
+ */
