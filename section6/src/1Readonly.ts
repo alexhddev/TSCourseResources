96386582951c3a53bfba1e30b53acb7f574e5efe
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-06-06 16:17:14 +0300

    Readonly and const

---
+++ section6/src/1Readonly.ts
@@ -0,0 +1,42 @@
+
+
+const CEO = 'Jeff' // strings are immutable 
+
+const CeoObject = { // objects are not immutable
+    name: 'Jeff',
+    company: 'Amazon'
+} as const
+
+// CeoObject.name = 'Bill'
+
+function toUpperCaseNames(names: ReadonlyArray<string>){
+    // names.push('My Name')
+    return names.map(name =>{
+        return name.toUpperCase()
+    })
+}
+
+
+type Position = 'Programmer' | 'Manager' | 'HR' | 'Scrum master'
+
+type Employee = {
+    name: string,
+    position: Position
+}
+
+function paySalary(empl: Employee){
+    console.log(`The pay for ${empl.position} is 10000 `)
+}
+
+const john = {
+    name: 'John',
+    position: 'Programmer'
+} as const
+
+paySalary(john)
+
+
+
+
+
+
