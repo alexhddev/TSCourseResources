9e8f71929c579627ee0194b9b3e4360bd1d64caa
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-06-06 16:51:16 +0300

    Record and index signatures

---
+++ section6/src/2Record.ts
@@ -0,0 +1,44 @@
+type Employee = {
+    name: string,
+    position: string,
+    [key: string]: string | number
+}
+
+const john: Employee = {
+    name: 'John',
+    position: 'programmer',
+    // extend the object?
+    email: 'john@company.com',
+    age: 30
+}
+
+// type EmployeeDictionary = {
+//     [id: string]: Employee
+// }
+
+const employeesRecords: Record<string, Employee> = {
+    abc: john
+}
+
+const myEmployee = employeesRecords['abc']
+
+// we get type safety for combining types:
+
+type Positions = 'Programmer' | 'Manager' | 'HR' | 'Scrum Master'
+
+type PositionsSalaries = Record<Positions, { salary: number }>
+
+const salaries: PositionsSalaries = {
+    Programmer: {
+        salary: 100000
+    },
+    HR: {
+        salary: 100000
+    },
+    Manager: {
+        salary: 100000
+    },
+    'Scrum Master': {
+        salary: 100000
+    }
+} 
