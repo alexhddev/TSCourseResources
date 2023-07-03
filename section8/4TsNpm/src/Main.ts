8ec8b4aeeedee05810f8a67ddc095c0493f023d2
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-07-03 16:37:54 +0300

    TS and npm

---
+++ section8/4TsNpm/src/Main.ts
@@ -0,0 +1,8 @@
+import { concatenateStrings, concatenateStringsWithSpace } from "./Util";
+
+const nameAndFunction = concatenateStrings('John', 'Programmer');
+
+const nameAndFunctionWithSpace = concatenateStringsWithSpace('John', 'Programmer')
+
+console.log(nameAndFunction)
+console.log(nameAndFunctionWithSpace)
---
+++ section8/4TsNpm/src/Util.ts
@@ -0,0 +1,7 @@
+export const concatenateStrings = (arg1: string, arg2: string) =>{
+    return arg1 + arg2
+}
+
+export const concatenateStringsWithSpace = (arg1: string, arg2: string) =>{
+    return arg1 + ' ' +arg2
+}
