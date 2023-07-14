6e26e351755e3b42c95845076e9539a2ff14768b
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-07-14 18:26:52 +0300

    CLI options

---
+++ section9/6CliOptions/src/Main.ts
@@ -0,0 +1,20 @@
+function doStuffTimes(times: number): void{
+    for (let index = 0; index < times; index++) {
+        console.log('Doingzzzzz something' + index);        
+    }
+}
+
+interface Person {
+    name: string,
+    age: number
+}
+
+function printPerson(person: Person) {
+    console.log(`${person.name} is ${person.age} years old`)
+}
+
+doStuffTimes(3);
+printPerson({
+    name: 'John',
+    age: 30
+})
