8676cc7e15017e222a32ee5b859490325ad12f7b
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-08-17 09:57:34 +0300

    Linting

--- section14/src/Main.ts
+++ section14/src/Main.ts
@@ -4,7 +4,9 @@ import { toUpperCase } from "./Utils";
 class Main {
 
     public async start(){
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         const upperCase1 = await toUpperCase('upperCase1')
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         const otherExample = await toUpperCase('otherExample')
         console.log('End of execution')
     }
